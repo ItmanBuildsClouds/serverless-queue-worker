4e9af119d9a37aec24d222eb8045d9ea299941a3
ItmanBuildsClouds <itmanpiotr.js@gmail.com>
2025-09-06 12:59:08 +0200

    fix(backend): Rename resource local name of DynamoDB

--- backend/terraform/dynamodb.tf
+++ backend/terraform/dynamodb.tf
@@ -1,4 +1,4 @@
-resource "aws_dynamodb_table" "basic-dynamodb-table" {
+resource "aws_dynamodb_table" "order_table" {
   name           = "${var.project_name}-orders"
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "OrderID"
