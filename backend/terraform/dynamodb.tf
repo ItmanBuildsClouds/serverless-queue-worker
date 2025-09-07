9ce7dbecd412d7e68f86676565f019b8ecf0ee1e
ItmanBuildsClouds <itmanpiotr.js@gmail.com>
2025-09-07 13:57:55 +0200

    fix(dynamodb): Align primary key case with application code

--- backend/terraform/dynamodb.tf
+++ backend/terraform/dynamodb.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "order_table" {
   name           = "${var.project_name}-orders"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "orderID"
+  hash_key       = "orderId"
 
   attribute {
-    name = "orderID"
+    name = "orderId"
     type = "S"
   }
   tags = {
